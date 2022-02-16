067009856a6ad887c8786ba6d333a534cb1bd279
fabio-dias <fabio.carrilho.1996@gmail.com>
2022-02-16 18:10:34 +0000

    Apresentação dinâmica com PHP 1º versão

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+
+$bd_host = 'localhost';
+$bd_user = 'root';
+$bd_pass = '';
+$bd_name = 'ki-sabor';
+
+$conexao = mysqli_connect($bd_host, $bd_user, $bd_pass, $bd_name) or die("Impossível conectar ao localhost");
+
+
+?>
